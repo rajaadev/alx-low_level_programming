458d18e9816631db35baad17f40a474322580785
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-16 17:30:18 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints  message put function, a new line.
+ * Return:  0 
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+
+    return (0);
+}
