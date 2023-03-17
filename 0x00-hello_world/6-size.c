b443649e7ab00e79c43ee09abd7e656915fe8559
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-16 19:12:27 -0700

    size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -15,6 +15,6 @@ printf("Size of an int: %ld byte(s)\n",(unsigned long )sizeof(b));
 printf("Size of a long int: %ld byte(s)\n", (unsigned long )sizeof(c));
 printf("Size of a long long int: %ld byte(s)\n", (unsigned long )sizeof(d));
 printf("Size of a float: %ld byte(s)\n", (unsigned long )sizeof(f));
-return 0;
+return (0);
 }
 
