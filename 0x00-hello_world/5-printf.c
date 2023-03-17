d126b6b808e6f900e5b6db192f5084515a5c3cc4
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-16 18:30:31 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints  line printf function
+ * main - prints  message put function, a new line.
  * Return:  0
  */
 int main(void)
-{
-printf("Programming is like building a multilingual puzzle\n");
+printf("with proper grammar, but the outcome is a piece of art\n");
 return (0);
 }
 
