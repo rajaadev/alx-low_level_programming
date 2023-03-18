4a4961c8b638af770b1b9ab1e8dcb2f5e7dace06
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-17 17:37:59 -0700

    positive_or_negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,4 +1,3 @@
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
@@ -6,17 +5,25 @@
 
 /* betty style doc for function main goes there */
 /* The last digit */
-
+/**
+ * main -  Entry point of the program
+ * This function is the entry point
+ * of the program and is responsible for
+ * starting the execution of the program
+ * It initializes any necessary resources
+ * and then calls other functions as needed.
+ * Return: 0 on success, non-zero on failure
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-/* function main Positive or negative following the value of n.*/
+
 	if (n == 0)
 	{
-		printf("%d is zero\n", n);
+	printf("%d is zero\n", n);
 	}
 	else if (n > 0)
 	{
@@ -28,4 +35,3 @@ int main(void)
 	}
 	return (0);
 }
-
