7b4458475363631e52f869a54bb74053493a6d5e
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-17 18:24:02 -0700

    last_digit

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main -  Entry point of the program
+ * This function is the entry point
+ * of the program and is responsible for
+ * starting the execution of the program
+ * It initializes any necessary resources
+ * and then calls other functions as needed.
+ * Return: 0 on success, non-zero on failure
+ */
+int main(void)
+{
+	int n;
+	int ld;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	ld = n % 10;
+	if (n > 5)
+	{
+	printf("Last digit of%d is %d greater than 5\n", n, ld);
+	}
+	else if (n == 0)
+	{
+	printf("Last digit of%d is %d and is 0\n", n, ld);
+	}
+	else
+	{
+	printf("Last digit of%d and is %d less than 6 and not 0\n", n, ld);
+	}
+	return (0);
+}
