e0c8f71a03bce26f87244ee09e728d7ce9dd5d99
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-17 20:17:29 -0700

    print_numb

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * This function is the entry point
+ * of the program and is responsible for
+ * starting the execution of the program.
+ * It initializes any necessary resources
+ * and then calls other functions as needed.
+ * Return: 0 on success, non-zero on failure
+ */
+int main(void)
+{
+	int  num = 0;
+	/* print single digit numbers */
+	while (num < 10)
+	{
+	putchar (num +'0');
+	num++;
+	}
+	putchar ('\n');
+	return (0);
+}
