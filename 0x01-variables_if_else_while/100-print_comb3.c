7fe05d654478c700b7f7837a77c2d249caee92c3
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-19 04:06:13 -0700

    print_comb

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,15 +12,15 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0 ; i < 10 ; i++)
+	for (i = 0 ; i < 9 ; i++)
 	{
-		for (j = 0 ; j < 10 ; j++)
-		{
-		putchar (i + 48);
-		putchar (j + 48);
-		putchar (',');
-		putchar (32);
-		}
+	for (j = i + 1 ; j < 10 ; j++)
+	{
+	putchar (i + 48);
+	putchar (j + 48);
+	putchar (',');
+	putchar (32);
+	}
 	}
 	return (0);
 }
