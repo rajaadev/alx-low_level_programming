0e21faed79699a2ecc2c7b078585c5cd375e101b
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-20 14:52:50 -0700

    print_comb5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ * This function is the entry point
+ * of the program and is responsible for
+ * starting the execution of the program.
+ * It initializes any necessary resources
+
+ * and then calls other functions as needed.
+ * Return: 0 on success, non-zero on failure
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 99; i++)
+	{
+	for (j = i + 1 ; j < 100; j++)
+	{
+	int ffd = i / 10;
+	int fsd = i % 10;
+	int sfd = j / 10;
+	int ssd = j % 10;
+
+	putchar(ffd + 48);
+	putchar(fsd  + 48);
+	putchar(' ');
+	putchar(sfd  + 48);
+	putchar(ssd + 48);
+
+	if (i == 98 && j == 99)
+	{
+	}
+	else
+	{
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}
