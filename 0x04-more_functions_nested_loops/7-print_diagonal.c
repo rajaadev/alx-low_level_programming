01f754a9dc80e12f151a77a85b06bb537696abd9
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-24 18:05:52 -0700

    print__diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,20 +6,18 @@
  */
 
 void print_diagonal(int n)
-	{
+{
 	int position_n, space;
 
 	if (n <= 0)
-	{	
-	_putchar('\n');
-	}	
+		_putchar('\n');
 	else
-	{	
+	{
 		for (position_n = 1; position_n  <= n; position_n++)
 		{
 			for (space = 1; space <= position_n; space++)
 				_putchar(' ');
-			_putchar(92);
+			_putchar(92);/* '\'*/
 			_putchar('\n');
 		}
 	}
