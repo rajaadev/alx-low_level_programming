169d90065b884ee7fc9e4c42128fbff038724b00
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-28 17:21:49 -0700

    atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,16 +2,22 @@
 /**
  * _atoi - function that convert a string to an integer
  * @s: string, numbers, characters
- * Return: integer
+ * Return: number integer
  */
 int _atoi(char *s)
 {
-int i = 0, d = 0, n = 0, len = 0, f = 0, digit = 0;
+int i = 0;
+int d = 0;
+int n = 0;
+int len = 0;
+int f = 0;
+int digit = 0;
+
 while (s[len] != '\0')
 len++;
 while (i < len && f == 0)
 {
-if (s[i] == 0)
+if (s[i] == '-')
 ++d;
 if (s[i] >= '0' && s[i] <= '9')
 {
