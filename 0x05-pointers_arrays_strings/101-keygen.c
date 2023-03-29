ea2831b372215fbb35960add1f7212718b17a81b
rajaadev <rajaa.elmorabet@gmail.com>
2023-03-28 18:02:34 -0700

    keygen

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -3,22 +3,31 @@
 #include <time.h>
 
 /**
- * main - generates random password for 101-crackme
+ * main - generates random
+ * password for 101-crackme
  * Return: 0
  */
 
 int main(void)
 {
-int sum;
-char c;
+int i, n;
+int sum = 0;
+int pass[100];
 
 srand(time(NULL));
-while (sum <= 2645)
+
+for (i = 0; i < 100; i++)
 {
-c = rand() % 128;
-sum += c;
-putchar(c);
+pass[i] = rand() % 78;
+sum += (pass[i] + '0');
+putchar(pass[i] + '0');
+if (2772 - sum - '0' < 78)
+{
+n = 2772 - sum - '0';
+sum += n;
+putchar(n + '0');
+break;
 }
-putchar(2772 - sum);
+}
 return (0);
 }
