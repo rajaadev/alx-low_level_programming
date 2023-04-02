ba1bcdd19a5bee6406916b6cfa12fe152f6d5c6b
rajaadev <rajaa.elmorabet@gmail.com>
2023-04-02 15:30:01 -0700

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* positive_or_negative - function  design number positif or negatif
+* @i : integer
+*/
+void positive_or_negative(int i)
+{
+if ( i >= 0 )
+printf("%d is positif\n",i);
+else
+printf("%d is negatiff\n",i);
+}
