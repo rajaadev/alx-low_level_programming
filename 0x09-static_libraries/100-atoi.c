3cf767a2a3ea354f886800d021896f4373080200
rajaadev <rajaa.elmorabet@gmail.com>
2023-04-10 15:05:21 -0700

    sl

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,26 +1,38 @@
 #include "main.h"
 /**
- * _atoi - convert a string into an integer.
- * @s: the string to use.
- * Return: integer.
+ * _atoi - function that convert a string to an integer
+ * @s: string, numbers, characters
+ * Return: number integer
  */
 int _atoi(char *s)
 {
- 	int sign = 1, i = 0;
- 	unsigned int res = 0;
+int i = 0;
+int d = 0;
+int n = 0;
+int len = 0;
+int f = 0;
+int digit = 0;
 
- 	while (!(s[i] <= '9' && s[i] >= '0') && s[i] != '\0')
- 	{
- 		if (s[i] == '-')
- 			sign *= -1;
- 		i++;
- 	}
- 	while (s[i] <= '9' && (s[i] >= '0' && s[i] != '\0'))
- 	{
- 		res = (res * 10) + (s[i] - '0');
- 		i++;
- 	}
- 	res *= sign;
- 	return (res);
- }
-
+while (s[len] != '\0')
+len++;
+while (i < len && f == 0)
+{
+if (s[i] == '-')
+++d;
+if (s[i] >= '0' && s[i] <= '9')
+{
+digit = s[i] - '0';
+if (d % 2)
+digit = -digit;
+n = n * 10 + digit;
+f = 1;
+if (s[i + 1] < '0' || s[i + 1] > '9')
+break;
+f = 0;
+}
+i++;
+}
+if (f == 0)
+return (0);
+return (n);
+}
