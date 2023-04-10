b52f1f481fc2cf0bbca77a5c0c552ba07e78db6f
rajaadev <rajaa.elmorabet@gmail.com>
2023-04-10 11:49:20 -0700

    static lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,14 +1,13 @@
 #include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-}
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
+/**
+ *_puts : function that print char
+ */
+void _puts(char *s)
+{
+puts(s);
+}
