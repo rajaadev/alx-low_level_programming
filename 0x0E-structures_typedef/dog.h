6aa2dcaeada8632b4c5301949c5391d0101422f4
rajaadev <rajaa.elmorabet@gmail.com>
2023-04-17 17:41:39 -0700

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog -  structure declaration
+* @name: member 1
+* @age: member 2
+* @owner: member 3
+* Description structure
+*/
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+#endif
