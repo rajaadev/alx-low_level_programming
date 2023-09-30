239b7ffd98ae903521a9be5b77fbff163eb06943
Rajaa El Mor <rajaa.elmorabet@gmail.com>
2023-09-29 18:05:21 -0700

    sets the value of a bit to 1 at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,18 +2,16 @@
 /**
  * set_bit - sets a bit at the index
  * @n: pointer to the number to index
- * @index: * the bit to set 
+ * @index: * the bit to set
  *
  * Return: 1 for success, -1 for error
  */
-int set_bit(unsigned long int *n, unsigned int index) {
-if (index >= sizeof(unsigned long int) *8)
+int set_bit(unsigned long int *n, unsigned int index)
 {
-return -1; // Index out of range
+if (index >= sizeof(n) * 8)
+{
+return (-1);
 }
-
-unsigned long int mask = 1UL << index;
-*n |= mask;
-
-return 1; // Bit set successfully
+*n |= 1UL << index;
+return (1);
 }
