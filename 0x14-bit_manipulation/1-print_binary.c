22428fbd2f5329532dde9928d72d2e2e04fe630c
Rajaa El Mor <rajaa.elmorabet@gmail.com>
2023-09-29 17:05:40 -0700

    convert decimal to binart

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -4,11 +4,27 @@
  * print_binary - convert decimal number to binary and print it.
  * @number: number to print in binary
  */
+
 void print_binary(unsigned long int number)
 {
-	if (number > 1)
-	{
-		print_binary(number >> 1);
-	}
-	printf("%lu", number & 1);
+int i;
+int count = 0;
+unsigned long int current_number;
+
+for (i = 63 ; i >= 0; i--)
+{
+current_number =  number >> i;
+
+if (current_number & 1)
+{
+_putchar('1');
+count++;
+}
+else if (count)
+
+_putchar('0');
+}
+if (!count)
+_putchar('0');
+
 }
