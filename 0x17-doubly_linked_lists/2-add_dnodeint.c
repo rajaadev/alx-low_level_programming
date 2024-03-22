9718d4d26c3e4f5d1ad1aa42291d9f9647edbcf2
Rajaa ElMor <rajaa.elmorabet@gmail.com>
2024-03-22 05:34:10 -0700

    add node integer

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,26 +1,24 @@
 #include "lists.h"
-
 /**
  * add_dnodeint - adds at head a new node of a dlistint
- * @h: pointer of head of the list
+ * @current_head: pointer of head of the list
  * @n: value of new node
- * Return: the address of the new element or Null
+ * Return: the address of the new element or Null if failed
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new = malloc(sizeof(dlistint_t));
-	dlistint_t *h = *head;
+	dlistint_t *current_head = *head;
 
 	if (new == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = h;
+	new->next = current_head;
 
-	if (h != NULL)
-		h->prev = new;
-
+	if (current_head != NULL)
+		(current_head)->prev = new;
 	*head = new;
 
 	return (new);
