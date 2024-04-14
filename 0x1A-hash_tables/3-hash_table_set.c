e2bcc511a7f074b55eab70b71e8f00bd3333a6f1
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-14 18:08:26 +0000

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -21,10 +21,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
     {
         if (strcmp(tmp->key, key) == 0)
         {
-            free(tmp->value);
-            tmp->value = strdup(value);
-            if (tmp->value == NULL)
+            char *new_value = strdup(value);
+            if (new_value == NULL)
                 return (0);
+            free(tmp->value);
+            tmp->value = new_value;
             return (1);
         }
         tmp = tmp->next;
