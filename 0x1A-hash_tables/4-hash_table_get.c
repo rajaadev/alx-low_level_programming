69f8897732830f0aedf86f5ef7c7f78d6c483d88
Rajaa <rajaa.elmorabet@gmail.com>
2024-04-14 14:10:58 +0000

    hst get

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,6 +1,4 @@
 #include "hash_tables.h"
-
-
 /**
  * hash_table_get - Retrieves a value for key.
  * @ht: The hash table to look into
@@ -9,21 +7,20 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index;
-	hash_node_t *current;
+        unsigned long int index;
+        hash_node_t *current;
 
-	if (ht == NULL || key == NULL || ht->array == NULL)
-		return (NULL);
+        if (ht == NULL || key == NULL || ht->array == NULL)
+                return (NULL);
 
-	index = key_index((const unsigned char *)key, ht->size);
-	current = ht->array[index];
+        index = key_index((const unsigned char *)key, ht->size);
+        current = ht->array[index];
 
-	while (current != NULL)
-	{
-		if (strcmp(current->key, key) == 0)
-			return (current->value);
-		current = current->next;
-	}
-
-	return (NULL);
+        while (current != NULL)
+        {
+                if (strcmp(current->key, key) == 0)
+                        return (current->value);
+                current = current->next;
+        }
+        return (NULL);
 }
