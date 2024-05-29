a8f7ffde24110317b7e2bc80f2682c8f1c933e25
Rajaa <rajaa.elmorabet@gmail.com>
2024-05-29 23:31:46 +0000

    makefile

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -19,14 +19,3 @@ def island_perimeter(grid):
                 if c > 0 and grid[r][c-1] == 1:  # Check left
                     perimeter -= 2
     return perimeter
-
-# Example usage
-if __name__ == "__main__":
-    grid = [
-        [0, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 1, 1, 1, 0, 0],
-        [0, 0, 0, 0, 0, 0]
-    ]
-    print(island_perimeter(grid))  # Output should be 12
