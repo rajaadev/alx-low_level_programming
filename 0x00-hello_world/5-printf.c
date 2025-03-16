65ddc2cf70169910a1fa19d479b203a27cafade2
Rajaa <rajaa.elmorabet@gmail.com>
2025-03-16 08:32:55 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ *
+ * Description: This function prints message to stdout
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
