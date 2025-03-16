344834a468a64afe5fcf264c3081bf820e2c63ae
Rajaa <rajaa.elmorabet@gmail.com>
2025-03-16 08:18:10 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Entry of the program
+ *
+ * Description: This function prints message to stdout
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle");
+return (0);
+}
