2c63591f0b3d57fa795138b6643fade7757f2cb1
Rajaa <rajaa.elmorabet@gmail.com>
2025-03-16 18:41:33 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,20 +1,17 @@
 #include <stdio.h>
+#include <unistd.h>
 /**
  * main - Entry point of the program
  *
  * Description: This program prints a quote by Dora Korpar
- * to standard output using putchar in a for loop
+ * to standard output using write
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char S[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char S[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	int i;
-
-	for (i = 0 ; S[i] != '\0' ; i++)
-		putchar(S[i]);
-putchar('\n');
-return (0);
+	write(2, S, sizeof(S) - 1);
+	return (0);
 }
