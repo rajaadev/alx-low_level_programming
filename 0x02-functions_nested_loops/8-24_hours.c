4c1f541ea355bd34d2b1ba4c27ad8a2ace098817
rajaa <rajaa.elmorabet@gmail.com>
2025-04-18 20:20:20 -0700

    8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -8,22 +8,17 @@
  */
 void jack_bauer(void)
 {
-	int h1 = 0;
-
-	int h2, m1, m2;
+	int h1, h2, m1, m2;
 
 	for (h1 = 0; h1 <= 2; h1++)
 	{
-		h2 = 0;
-
-		for (h2 = 0; h2 <= 3; h2++)
+		for (h2 = 0; h2 <= 9; h2++)
 		{
-			m1 = 0;
+			if (h1 == 2 && h2 > 3)
+                        	break;
 
 			for (m1 = 0; m1 <= 5; m1++)
 			{
-				m2 = 0;
-
 				for (m2 = 0; m2 <= 9; m2++)
 				{
 					_putchar(h1 + '0');
