65ff38a31622f8a35a2e804566bf21e93109bcb8
rajaa <rajaa.elmorabet@gmail.com>
2025-08-09 19:08:33 -0700

    1-listint_len.c

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns number elements in linked listint_t list
+ * @h: pointer to the head of the list
+ * Return: number of nodes in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
