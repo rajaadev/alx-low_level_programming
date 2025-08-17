277c3207ac3bba40ca6a6d32272fa6beea76efaa
rajaa <rajaa.elmorabet@gmail.com>
2025-08-16 20:29:29 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,51 +1,43 @@
 #include "main.h"
 
 /**
- * print_fibon_number - prints an unsigned long int number using _putchar.
- * Limits recursion depth to avoid stack overflow on very large numbers.
- * @high: high part (can be 0)
- * @low: low part
- * @print_full: print low with leading zeros if 1, else normal
+ * print_fibon_number - prints an unsigned long integer using _putchar.
+ * Prints numbers with fixed width if part of a big number.
+ * @high: the high part of the number (can be 0)
+ * @low: the low part of the number
  */
-void print_fibon_number(unsigned long high, unsigned long low, int print_full)
+void print_fibon_number(unsigned long high, unsigned long low)
 {
-	static int depth;
 	unsigned long div = 100000000;
 
 	if (high > 0)
 	{
-		if (depth++ < 10) /* recursion depth limit */
-			print_fibon_number(high, 0, 0);
+		/* Print high part normally */
+		print_fibon_number(0, high);
+
+		/* Print low part padded with leading zeros */
 		while (div > 0)
 		{
 			_putchar((low / div) % 10 + '0');
 			div /= 10;
 		}
-		depth--;
 	}
 	else
 	{
-		if (print_full)
-		{
-			while (div > 0)
-			{
-				_putchar((low / div) % 10 + '0');
-				div /= 10;
-			}
-		}
-		else
-		{
-			if (low / 10)
-			{
-				if (depth++ < 10)
-					print_fibon_number(0, low / 10, 0);
-				depth--;
-			}
-			_putchar((low % 10) + '0');
-		}
+		/* Print low part normally, without leading zeros */
+		if (low / 10)
+			print_fibon_number(0, low / 10);
+		_putchar((low % 10) + '0');
 	}
 }
 
+/**
+ * main - prints the first 98 Fibonacci numbers
+ *        starting with 1 and 2, separated by comma and space,
+ *        followed by a new line.
+ *
+ * Return: Always 0 (success)
+ */
 int main(void)
 {
 	unsigned long f1h = 0, f1l = 1;
@@ -53,12 +45,12 @@ int main(void)
 	unsigned long sumh, suml;
 	int count;
 
-	print_fibon_number(0, f1l, 0);
+	print_fibon_number(0, f1l);
 	_putchar(',');
 	_putchar(' ');
-	print_fibon_number(0, f2l, 0);
+	print_fibon_number(0, f2l);
 
-	for (count = 3; count <= 92; count++) /* safe limit */
+	for (count = 3; count <= 98; count++)
 	{
 		suml = f1l + f2l;
 		sumh = f1h + f2h;
@@ -70,15 +62,13 @@ int main(void)
 
 		_putchar(',');
 		_putchar(' ');
-		print_fibon_number(sumh, suml, (sumh > 0));
+		print_fibon_number(sumh, suml);
 
 		f1h = f2h;
 		f1l = f2l;
 		f2h = sumh;
 		f2l = suml;
 	}
-
 	_putchar('\n');
 	return (0);
 }
-
