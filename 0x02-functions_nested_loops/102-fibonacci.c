a10206e3f1c532dac1f511b5aa1f216dd894ec04
rajaa <rajaa.elmorabet@gmail.com>
2025-08-16 19:56:28 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -4,17 +4,16 @@
  * print_number - prints an integer using _putchar.
  * @n: the number to print.
  */
-void print_number(unsigned long n)
+void print_number_unsigned(unsigned long n)
 {
 	if (n / 10)
-		print_number(n / 10);
+		print_number_unsigned(n / 10);
 	_putchar((n % 10) + '0');
 }
 
 /**
  * main - prints the first 50 Fibonacci numbers starting with 1 and 2,
- * separated by comma and space, followed by a newline.
- *
+ * separated by comma & space, followed by a newline.
  * Return: Always 0.
  */
 int main(void)
@@ -22,17 +21,17 @@ int main(void)
 	unsigned long fib1 = 1, fib2 = 2, next;
 	int count;
 
-	print_number(fib1);
+	print_number_unsigned(fib1);
 	_putchar(',');
 	_putchar(' ');
-	print_number(fib2);
+	print_number_unsigned(fib2);
 
 	for (count = 3; count <= 50; count++)
 	{
 		next = fib1 + fib2;
 		_putchar(',');
 		_putchar(' ');
-		print_number(next);
+		print_number_unsigned(next);
 		fib1 = fib2;
 		fib2 = next;
 	}
