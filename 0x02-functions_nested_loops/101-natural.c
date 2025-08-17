aba762c4038d1e3196c25b826e896dfe3f4aa760
rajaa <rajaa.elmorabet@gmail.com>
2025-08-16 19:28:05 -0700

    101-natural

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,23 +1,33 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
+ * print_number - prints an integer using _putchar
+ * @n: the number to print
+ */
+void print_number(int n)
+{
+	if (n / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * main - computes and prints the sum of all multiples of 3 or 5 below 1024
  *
- *
- *
+ * Return: Always 0.
  */
 int main(void)
 {
-int i = 0;
-int s = 0;
+	int i, sum = 0;
 
-while(s < 1024)
+	for (i = 0; i < 1024; i++)
 	{
-	i++;
-	if (i / 3 == 0 || i / 5 == 0)
-	{
-		s = s+i;
-		printf("%d ,",s);
+		if ((i % 3 == 0) || (i % 5 == 0))
+			sum += i;
 	}
-	}
-return 0;
+
+	print_number(sum);
+	_putchar('\n');
+
+	return (0);
 }
